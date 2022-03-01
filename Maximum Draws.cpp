16059ca4968b45a73e5562f9aebf95a0952c218c
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2022-03-01 11:38:05 +0530

    Create Maximum Draws.cpp

---
+++ Maximum Draws.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n,a;
+    cin >> n;
+    while(n --){
+        cin >> a;
+        cout << a+1 << endl;
+    }
+    return 0;
+}
